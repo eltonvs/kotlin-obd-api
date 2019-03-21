592737b23f8814e72e49556df0ff2a5032bbfa1a
Elton Viana <eltonvs@outlook.com>
2019-03-20 23:58:07 -0300

    ObdCommand revamp
    Change from interface to a class and change main structure

--- command/src/main/kotlin/com/github/eltonvs/obd/command/ObdCommand.kt
+++ command/src/main/kotlin/com/github/eltonvs/obd/command/ObdCommand.kt
@@ -3,16 +3,22 @@ package com.github.eltonvs.obd.command
 import com.github.eltonvs.obd.domain.ObdRawResponse
 import com.github.eltonvs.obd.domain.ObdResponse
 
-interface ObdCommand {
-    val name: String
-    val mode: String
-    val pid: String
+abstract class ObdCommand {
+    abstract val tag: String
+    abstract val name: String
+    abstract val mode: String
+    abstract val pid: String
 
-    fun handle(rawResponse: ObdRawResponse): ObdResponse =
+    open val defaultUnit: String = ""
+    open val handler: (String) -> String = { x: String -> x }
+
+    fun handleResponse(rawResponse: ObdRawResponse): ObdResponse =
         ObdResponse(
             command = this,
             rawResponse = rawResponse,
-            value = rawResponse.value
+            value = handler(rawResponse.value),
+            unit = defaultUnit
         )
-    fun format(response: ObdResponse): String = "${response.value}${response.unit}"
+
+    open fun format(response: ObdResponse): String = "${response.value}${response.unit}"
 }
