9fb9d812d01749359d3e70e61061535f105fd033
Elton Viana <eltonvs@outlook.com>
2019-03-26 01:16:53 -0300

    Add bufferedValue field to ObdRawResponse

--- domain/src/main/kotlin/com/github/eltonvs/obd/domain/ObdRawResponse.kt
+++ domain/src/main/kotlin/com/github/eltonvs/obd/domain/ObdRawResponse.kt
@@ -3,4 +3,7 @@ package com.github.eltonvs.obd.domain
 data class ObdRawResponse(
     val value: String,
     val elapsedTime: Long
-)
+) {
+    val bufferedValue: IntArray
+        get() = value.chunked(2) { it.toString().toInt(radix = 16) }.toIntArray()
+}
