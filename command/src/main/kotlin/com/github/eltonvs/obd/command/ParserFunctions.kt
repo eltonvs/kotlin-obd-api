bdf8b3842d7f9fae09550c8c45d1848d1e7edbfa
Elton Viana <eltonvs@outlook.com>
2019-03-26 01:17:24 -0300

    Create parser functions file

---
+++ command/src/main/kotlin/com/github/eltonvs/obd/command/ParserFunctions.kt
@@ -0,0 +1,11 @@
+package com.github.eltonvs.obd.command
+
+import kotlin.math.pow
+
+
+fun bytesToInt(bufferedValue: IntArray, start: Int = 2): Long =
+    bufferedValue.drop(start).foldIndexed(0L) { index, total, current ->
+        total + current * 2f.pow((bufferedValue.size - start - index - 1) * 8).toLong()
+    }
+
+fun calculatePercentage(bufferedValue: IntArray): Float = (bytesToInt(bufferedValue) * 100f) / 255f
