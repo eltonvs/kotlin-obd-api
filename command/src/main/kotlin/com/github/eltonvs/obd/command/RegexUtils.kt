4b80acddb0056be3208421d88411f048c61dbe05
Elton Viana <eltonvs@outlook.com>
2019-04-04 02:21:58 -0300

    Pre-process response raw value, cleaning string and checking for some errors

--- command/src/main/kotlin/com/github/eltonvs/obd/command/RegexUtils.kt
+++ command/src/main/kotlin/com/github/eltonvs/obd/command/RegexUtils.kt
@@ -4,9 +4,13 @@ import java.util.regex.Pattern
 
 
 object RegexPatterns {
+    val WHITESPACE_PATTERN: Pattern = Pattern.compile("\\s")
+    val BUS_INIT_PATTERN: Pattern = Pattern.compile("(BUS INIT)|(BUSINIT)|(\\.)")
     val SEARCHING_PATTERN: Pattern = Pattern.compile("SEARCHING")
     val CARRIAGE_PATTERN: Pattern = Pattern.compile("[\r\n]")
     val CARRIAGE_COLON_PATTERN: Pattern = Pattern.compile("[\r\n].:")
+    val COLON_PATTERN: Pattern = Pattern.compile(":")
+    val DIGITS_LETTERS_PATTERN: Pattern = Pattern.compile("([0-9A-F])+")
     val STARTS_WITH_ALPHANUM_PATTERN: Pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE)
 }
 
--- command/src/main/kotlin/com/github/eltonvs/obd/command/Response.kt
+++ command/src/main/kotlin/com/github/eltonvs/obd/command/Response.kt
@@ -1,12 +1,58 @@
 package com.github.eltonvs.obd.command
 
+import com.github.eltonvs.obd.command.RegexPatterns.BUS_INIT_PATTERN
+import com.github.eltonvs.obd.command.RegexPatterns.COLON_PATTERN
+import com.github.eltonvs.obd.command.RegexPatterns.DIGITS_LETTERS_PATTERN
+import com.github.eltonvs.obd.command.RegexPatterns.WHITESPACE_PATTERN
+
+
+fun <T> T.pipe(vararg functions: (T) -> T): T =
+    functions.fold(this) { value, f -> f(value) }
+
 
 data class ObdRawResponse(
     val value: String,
     val elapsedTime: Long
 ) {
+    private val valueProcessorPipeline by lazy {
+        arrayOf<(String) -> String>(
+            {
+                /*
+                 * Imagine the following response 41 0c 00 0d.
+                 *
+                 * ELM sends strings!! So, ELM puts spaces between each "byte". And pay
+                 * attention to the fact that I've put the word byte in quotes, because 41
+                 * is actually TWO bytes (two chars) in the socket. So, we must do some more
+                 * processing...
+                 */
+                removeAll(WHITESPACE_PATTERN, it) // removes all [ \t\n\x0B\f\r]
+            },
+            {
+                /*
+                 * Data may have echo or informative text like "INIT BUS..." or similar.
+                 * The response ends with two carriage return characters. So we need to take
+                 * everything from the last carriage return before those two (trimmed above).
+                 */
+                removeAll(BUS_INIT_PATTERN, it)
+            },
+            {
+                removeAll(COLON_PATTERN, it)
+            },
+            {
+                if (!DIGITS_LETTERS_PATTERN.matcher(it).matches()) {
+                    // Non Numeric Response Exception
+                    throw Exception(it)
+                }
+                it
+            }
+        )
+    }
+
+    private val processedValue: String
+        get() = value.pipe(*valueProcessorPipeline)
+
     val bufferedValue: IntArray
-        get() = value.chunked(2) { it.toString().toInt(radix = 16) }.toIntArray()
+        get() = processedValue.chunked(2) { it.toString().toInt(radix = 16) }.toIntArray()
 }
 
 data class ObdResponse(
