959d773ef8fbd1abf3a7f09e4f77d336e3fe51eb
Elton Viana <me@eltonviana.com>
2022-06-08 09:52:03 -0400

    Fix EGR commands

--- src/main/kotlin/com/github/eltonvs/obd/command/egr/Egr.kt
+++ src/main/kotlin/com/github/eltonvs/obd/command/egr/Egr.kt
@@ -6,21 +6,21 @@ import com.github.eltonvs.obd.command.bytesToInt
 import com.github.eltonvs.obd.command.calculatePercentage
 
 class CommandedEgrCommand : ObdCommand() {
-  override val tag = "COMMANDED_EGR"
-  override val name = "Commanded EGR"
-  override val mode = "01"
-  override val pid = "2C"
+    override val tag = "COMMANDED_EGR"
+    override val name = "Commanded EGR"
+    override val mode = "01"
+    override val pid = "2C"
 
-  override val defaultUnit = "%"
-  override val handler = { it: ObdRawResponse -> "%.1f".format(calculatePercentage(it.bufferedValue)) }
+    override val defaultUnit = "%"
+    override val handler = { it: ObdRawResponse -> "%.1f".format(calculatePercentage(it.bufferedValue)) }
 }
 
 class EgrErrorCommand : ObdCommand() {
-  override val tag = "EGR_ERROR"
-  override val name = "EGR Error"
-  override val mode = "01"
-  override val pid = "2D"
+    override val tag = "EGR_ERROR"
+    override val name = "EGR Error"
+    override val mode = "01"
+    override val pid = "2D"
 
-  override val defaultUnit = "%"
-  override val handler = { it: ObdRawResponse -> "%.1f".format(bytesToInt(it.bufferedValue) * (100f / 128) - 100) }
+    override val defaultUnit = "%"
+    override val handler = { it: ObdRawResponse -> "%.1f".format(bytesToInt(it.bufferedValue, bytesToProcess = 1) * (100f / 128) - 100) }
 }
--- src/test/kotlin/com/github/eltonvs/obd/command/egr/Egr.kt
+++ src/test/kotlin/com/github/eltonvs/obd/command/egr/Egr.kt
@@ -8,47 +8,48 @@ import kotlin.test.assertEquals
 
 @RunWith(Parameterized::class)
 class CommandedEgrCommandParameterizedTests(private val rawValue: String, private val expected: Float) {
-  companion object {
-    @JvmStatic
-    @Parameterized.Parameters
-    fun values() = listOf(
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun values() = listOf(
             arrayOf("414545", 27.1f),
             arrayOf("414500", 0f),
             arrayOf("4145FF", 100f)
-    )
-  }
-
-  @Test
-  fun `test valid commanded egr responses handler`() {
-    val rawResponse = ObdRawResponse(value = rawValue, elapsedTime = 0)
-    val obdResponse = CommandedEgrCommand().run {
-      handleResponse(rawResponse)
+        )
     }
-    assertEquals("%.1f".format(expected) + '%', obdResponse.formattedValue)
-  }
+
+    @Test
+    fun `test valid commanded egr responses handler`() {
+        val rawResponse = ObdRawResponse(value = rawValue, elapsedTime = 0)
+        val obdResponse = CommandedEgrCommand().run {
+            handleResponse(rawResponse)
+        }
+        assertEquals("%.1f".format(expected) + '%', obdResponse.formattedValue)
+    }
 }
 
 @RunWith(Parameterized::class)
 class EgrErrorCommandParameterizedTests(private val rawValue: String, private val expected: Float) {
-  companion object {
-    @JvmStatic
-    @Parameterized.Parameters
-    fun values() = listOf(
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun values() = listOf(
             arrayOf("410610", -87.5f),
             arrayOf("410643", -47.7f),
             arrayOf("410680", 0f),
             arrayOf("4106C8", 56.25f),
             arrayOf("410600", -100f),
-            arrayOf("4106FF", 99.2f)
-    )
-  }
-
-  @Test
-  fun `test valid egr error responses handler`() {
-    val rawResponse = ObdRawResponse(value = rawValue, elapsedTime = 0)
-    val obdResponse = EgrErrorCommand().run {
-      handleResponse(rawResponse)
+            arrayOf("4106FF", 99.2f),
+            arrayOf("4106FFFF", 99.2f)
+        )
     }
-    assertEquals("%.1f".format(expected) + '%', obdResponse.formattedValue)
-  }
+
+    @Test
+    fun `test valid egr error responses handler`() {
+        val rawResponse = ObdRawResponse(value = rawValue, elapsedTime = 0)
+        val obdResponse = EgrErrorCommand().run {
+            handleResponse(rawResponse)
+        }
+        assertEquals("%.1f".format(expected) + '%', obdResponse.formattedValue)
+    }
 }
